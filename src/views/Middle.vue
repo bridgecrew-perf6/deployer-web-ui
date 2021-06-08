db3d9dc8d99eafd763e3a3c3d049945875b8a87d
suixian.gong <suixian.gong@sumscope.com>
2021-06-08 14:48:21 +0800

    middle

--- src/views/Middle.vue
+++ src/views/Middle.vue
@@ -3,9 +3,25 @@
 </template>
 
 <script lang="ts">
+import deployerRepository from "@/api/deployerRepository";
+import router from "@/router";
+
 export default {
   name: "Middle",
   setup() {
+    const token = localStorage.getItem('token')
+
+    const login = async () => {
+      const data = await deployerRepository.login()
+      await localStorage.setItem('token', data.token)
+      await router.push('/cd/home').then()
+    }
+
+    if (token) {
+      router.push('/cd/home')
+    } else {
+      login()
+    }
 
     return {
 
