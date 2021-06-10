2c5c1c7f5d5ad4b3fa3fb706d25d27f04cf08d8c
suixian.gong <suixian.gong@sumscope.com>
2021-06-10 12:04:24 +0800

    any

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
   rules: {
     "@typescript-eslint/explicit-function-return-type": 'off',
     "@typescript-eslint/explicit-module-boundary-types": 'off',
+    "@typescript-eslint/no-explicit-any": ["off"]
   }
 }
