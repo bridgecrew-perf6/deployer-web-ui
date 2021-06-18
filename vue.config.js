67eb1b931a08bdb93bedda3d30f81964e72cfd4f
suixian.gong <suixian.gong@sumscope.com>
2021-06-18 18:51:04 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,14 @@ const vueConfig = {
     publicPath: '/',
     outputDir: 'dist',
     devServer: {
-        host: '0.0.0.0',
-        port: 4204,
+        disableHostCheck: true,
+        host: '127.0.0.1',
+        port: 7006,
         open: true,
         proxy: {
             '/api/v1': {
-                target: 'http://deployer-web.dev.ops.sumscope.com:7006',
-                // target: 'http://127.0.0.1:8003',
+                // target: 'http://deployer-web.dev.ops.sumscope.com:7006',
+                target: 'http://172.16.97.194:7006',
                 changeOrigin: true,
             },
             '/api/my/': {
