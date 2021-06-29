382584c3aef93b8377f6f5e6f116cd3514559636
suixian.gong <suixian.gong@sumscope.com>
2021-06-29 15:13:27 +0800

    zn

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <a-config-provider :locale="zhCN">
+  <a-config-provider :locale="locale === 'zh' ? zhCN : enUS">
+<!--    <a-switch v-model:checked="checked" />-->
     <router-view></router-view>
   </a-config-provider>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent, ref, watch} from 'vue';
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import enUS from 'ant-design-vue/es/locale/en_US';
 
 export default defineComponent({
   name: 'App',
   components: {
   },
   setup() {
+    const locale = ref('zh')
+    const checked = ref(true)
+    watch(checked, (value) => {
+      locale.value = value ? 'zh' : 'en'
+    })
     return {
-      zhCN
+      enUS,
+      zhCN,
+      locale,
+      checked,
     }
   }
 });
